2e5d53260ba2041651ab45d0ae9397be28923586
HealthHelpProject <lchaitanya530@gmail.com>
2025-07-03 11:12:04 +0530

    First Commit 2

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Login from './routes/Login.jsx'
+import Register from './routes/Register.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App/>} />
+       <Route path='login' element={<Login />} />
+       <Route path='register' element={<Register />} />
+    </Routes>
+  </BrowserRouter>,
+)
